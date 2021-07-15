5fc1acdae83923b5d383ca6bf8f0598ab726fcf5
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-07-15 12:51:40 -0300

    created to add external font in project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, NextScript, Main } from 'next/document'
+
+import theme from '../styles/theme'
+
+export default class MyDocument extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        </Head>
+
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
