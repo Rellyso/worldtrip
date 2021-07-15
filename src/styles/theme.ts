64409fa32687558075b14f94b4ec6cba7f474653
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-07-15 12:51:06 -0300

    adjusted app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+  colors: {
+    heading: {
+      light: "#F5F8FA",
+      dark: "#47585B",
+    },
+    highlight: "#FFBA08",
+  },
+  styles: {
+    global: {
+      body: {
+        padding: 0,
+        margin: 0,
+      }
+    },
+
+  }
+})
+
+export default theme;
