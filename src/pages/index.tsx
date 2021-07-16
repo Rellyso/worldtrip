2b031d08d63763fb7048a2845802072eb1208dc6
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-07-16 13:42:26 -0300

    updated index page to add slider and more

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Grid, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import Head from 'next/head'
+import { Divider, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
 import { Container } from '../components/Container'
+import { Slider } from '../components/Slider'
 import { TravelTypeCard } from '../components/TravelTypeCard'
+
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         as="header"
         h={100}
         bg="heading.white"
-        w="100vw"
+        w="100%"
         m="0"
         p="0"
         align="center"
@@ -21,12 +22,16 @@ export default function Home() {
 
       <Container>
         <Flex
+          as="main"
           w="100%"
           bg="white"
           minH="300px"
           position="relative"
           bgImg="assets/images/sky-background.png"
+          bgAttachment="fixed"
           bgRepeat="no-repeat"
+          bgSize="contain"
+          bgPosition="top center"
           px={140}
           color="white"
         >
@@ -60,13 +65,43 @@ export default function Home() {
           />
         </Flex>
 
-        <Flex py="20" gap="4" px="24" justify="space-around">
+        <Flex as="section" py="24" px="24" justify="space-around">
           <TravelTypeCard src="assets/cocktail.svg">vida noturna</TravelTypeCard>
           <TravelTypeCard src="assets/surf.svg">praia</TravelTypeCard>
           <TravelTypeCard src="assets/building.svg">moderno</TravelTypeCard>
           <TravelTypeCard src="assets/museum.svg">clássico</TravelTypeCard>
           <TravelTypeCard src="assets/earth.svg">e mais...</TravelTypeCard>
         </Flex>
+
+        <Divider w={90} borderWidth="2px" borderColor="heading.dark" />
+
+        <Flex
+          as="section"
+          direction="column"
+          w="100%"
+        >
+          <Heading as="h3" py="14" fontWeight="medium" color="heading.dark">
+            Vamos nessa? <br />
+            Então escolha seu continente
+          </Heading>
+
+          <Slider
+            continents={[
+              {
+                name: 'América do Norte',
+                description: 'O continente do desenvolvimento',
+                image: 'https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+              },
+              {
+                name: 'Europa',
+                description: 'O continente mais antigo',
+                image: 'https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+              },
+            ]}
+          />
+        </Flex>
+
+
       </Container>
     </Flex >
   )
