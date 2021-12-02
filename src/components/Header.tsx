b39d97274a1ae996f90132e2b55ef3f42f103af9
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-12-02 20:19:20 -0300

    updated Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,40 @@
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { Flex, Image } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { Link } from "./Link";
 
 export function Header() {
+  const [pathname, setPathname] = useState("/")
+
+  useEffect(() => {
+    setPathname(window.location.pathname)
+    return () => {
+      setPathname("/")
+    }
+  }, [])
+
   return (
     <Flex
       as="header"
       h={100}
-      bg="heading.white"
+      bg="heading.light"
       w="100%"
       maxW={1440}
       mx="auto"
       p="0"
       align="center"
-      justify="space-between"
+      justify="center"
+      position="relative"
     >
-      <ChevronLeftIcon fontSize="32px" color="heading.dark" />
+      {pathname !== "/" && (
+        <Link href="/" h="100%" position="absolute" left="4" alignItems="center">
+          <ChevronLeftIcon fontSize="32px" color="heading.dark" />
+        </Link>
+      )}
 
-      <Image src="assets/logo.svg" />
-      <span />
+      <Link href="/" alignItems="center">
+        <Image src="/assets/logo.svg" />
+      </Link>
     </Flex>
   )
 }
